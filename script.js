efa2a6b55c5bb1b5571fb8d8092f365df45c0572
jupac1053 <jupac1053799430@gmail.com>
2024-10-30 11:18:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,28 +8,28 @@ const checkboxes = document.querySelectorAll('.checkbox input');
 
 // Función para enviar datos a Adafruit IO
 function sendToAdafruit(feedId, value) {
-    const url = `${adafruitIoUrl}${adafruitIoUsername}/feeds/${feedId}/data`;
-    const headers = {
-        'Content-Type': 'application/json',
-        'X-AIO-Key': adafruitIoKey,
-    };
-    const data = { value: value ? 1 : 0 };
+  const url = `${adafruitIoUrl}${adafruitIoUsername}/feeds/${feedId}/data`;
+  const headers = {
+    'Content-Type': 'application/json',
+    'X-AIO-Key': adafruitIoKey,
+  };
+  const data = { value: value ? 1 : 0 };
 
-    fetch(url, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(data),
-    })
-    .then((response) => response.json())
-    .then((data) => console.log(`Enviado a ${feedId}: ${value}`))
-    .catch((error) => console.error(`Error al enviar a ${feedId}: ${error}`));
+  fetch(url, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(data),
+  })
+  .then((response) => response.json())
+  .then((data) => console.log(`Enviado a ${feedId}: ${value}`))
+  .catch((error) => console.error(`Error al enviar a ${feedId}: ${error}`));
 }
 
 // Agrega eventos a los checkboxes
 checkboxes.forEach((checkbox, index) => {
-    checkbox.addEventListener('change', () => {
-        const feedId = feedIds[index];
-        const checked = checkbox.checked;
-        sendToAdafruit(feedId, checked);
-    });
+  checkbox.addEventListener('change', () => {
+    const feedId = feedIds[index];
+    const checked = checkbox.checked;
+    sendToAdafruit(feedId, checked);
+  });
 });
